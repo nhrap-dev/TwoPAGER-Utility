432c5299725c139c28d9d0dce220410635cf25c5
Raines II <0070642225@FEMA.DHS.GOV>
2020-08-12 11:19:23 -0500

    main error handling

--- TwoPAGER.py
+++ TwoPAGER.py
@@ -1,2 +1,14 @@
 from subprocess import call
-call('CALL conda.bat activate hazus_env & start /min python src\gui.py', shell=True)
+try:
+    try:
+        call('CALL conda.bat activate hazus_env & start /min python src\gui.py', shell=True)
+    except:
+        call('start /min python src\gui.py', shell=True)
+
+except:
+    import ctypes
+    import sys
+    messageBox = ctypes.windll.user32.MessageBoxW
+    error = sys.exc_info()[0]
+    messageBox(0, u"Unexpected error: {er} | If this problem persists, and you've ensured it's not user error, ask your developers to write better code.".format(
+        er=error), u"HazPy", 0x1000)
